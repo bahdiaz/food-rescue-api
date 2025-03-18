8262f475230fdf57cb358c5af8b0d39193e95070
Bárbara Dias <barbara.dias30@outlook.com>
2025-03-18 08:56:27 -0300

    Atualizações

--- src/main/java/br/com/food_rescue_api/controller/UserController.java
+++ src/main/java/br/com/food_rescue_api/controller/UserController.java
@@ -3,62 +3,65 @@ package br.com.food_rescue_api.controller;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import org.springframework.web.server.ResponseStatusException;
 
 import br.com.food_rescue_api.model.User;
 
 @RestController
+@RequestMapping("/users")
 public class UserController {
 
+    private Logger log = LoggerFactory.getLogger(getClass());
     private List<User> repository = new ArrayList<>();
 
-    @GetMapping("/users")
+    @GetMapping
     public List<User> index() {
         return repository;
     }
 
-    @PostMapping("/users")
+    @PostMapping
     public ResponseEntity<User> create(@RequestBody User user) {
-        System.out.println("Cadastrando..." + user);
+        log.info("Cadastrando usuário: " + user.getName());
         repository.add(user);
-        return ResponseEntity.status(201).body(user);
+        return ResponseEntity.status(HttpStatus.CREATED).body(user);
     }
 
-    @GetMapping("/users/{id}")
-    public ResponseEntity<User> get(@PathVariable Long id) {
-        System.out.println("Buscando usuário " + id);
-        var user = repository
+    @GetMapping("/{id}")
+    public User get(@PathVariable Long id) {
+        log.info("Buscando usuário " + id);
+        return getUser(id);
+    }
+
+    @PutMapping("/{id}")
+    public User update(@PathVariable Long id, @RequestBody User user) {
+        log.info("Atualizando usuário " + id + " com " + user);
+
+        repository.remove(getUser(id));
+        user.setId(id);
+        repository.add(user);
+
+        return user;
+    }
+
+    @DeleteMapping("/{id}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void destroy(@PathVariable Long id) {
+        log.info("Removendo usuário " + id);
+        repository.remove(getUser(id));
+    }
+
+    private User getUser(Long id) {
+        return repository
                 .stream()
                 .filter(u -> u.getId().equals(id))
-                .findFirst();
-
-        if (user.isEmpty()) {
-            return ResponseEntity.notFound().build();
-        }
-
-        return ResponseEntity.ok(user.get());
+                .findFirst()
+                .orElseThrow(
+                        () -> new ResponseStatusException(HttpStatus.NOT_FOUND, "Usuário não encontrado"));
     }
-
-    @DeleteMapping("/users/{id}")
-    public ResponseEntity<User> delete(@PathVariable Long id) {
-        System.out.println("Deletando usuário " + id);
-        var user = repository
-                .stream()
-                .filter(u -> u.getId().equals(id))
-                .findFirst();
-
-        if (user.isEmpty()) {
-            return ResponseEntity.notFound().build();
-        }
-
-        repository.remove(user.get());
-        return ResponseEntity.ok(user.get());
-    }
-
 }
--- src/main/java/br/com/food_rescue_api/model/User.java
+++ src/main/java/br/com/food_rescue_api/model/User.java
@@ -4,15 +4,13 @@ public class User {
 
     private Long id;
     private String name;
-    private String email;
 
     public User() {
     }
 
-    public User(Long id, String name, String email) {
+    public User(Long id, String name) {
         this.id = id;
         this.name = name;
-        this.email = email;
     }
 
     public Long getId() {
@@ -31,20 +29,8 @@ public class User {
         this.name = name;
     }
 
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
     @Override
     public String toString() {
-        return "User{" +
-                "id=" + id +
-                ", name='" + name + '\'' +
-                ", email='" + email + '\'' +
-                '}';
+        return "User{id=" + id + ", name='" + name + "'}";
     }
 }
